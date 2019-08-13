e1e7b67a67b34c5149a7ca19dfb7e9935a0753a9
StamatisDeli <stamatis.deliyannis@gmail.com>
2019-08-14 01:49:02 +0300

    1 file modified

--- Episodes.js
+++ Episodes.js
@@ -7,7 +7,7 @@ const Episodes = (props)=> {
     return (
       <section key={episode.id} className="episode-box">
         <img
-          // src={episode.image.medium}
+          src={episode.image.medium}
           alt={`Rick and Morty ${episode.name}`}
         />
         <div>{episode.name}</div>
